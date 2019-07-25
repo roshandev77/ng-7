ef5345b6b4f39853abf88a9defd095c4b3d08cf3
Roshan Kumar <roshan.kumar@intersection.com>
2019-07-25 15:20:56 +0530

    Application architecture created

---
+++ ng-material-exp/material-ng/src/app/material.ts
@@ -0,0 +1,9 @@
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+  })
+  export class MaterialModule { }
