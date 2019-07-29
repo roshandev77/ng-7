c73fd7c9faf0332f35c5aa5cfe1ccb6e9e9817f0
Roshan Kumar <roshan.kumar@intersection.com>
2019-07-29 11:54:58 +0530

    icon added in header

--- ng-material-exp/material-ng/src/app/material.ts
+++ ng-material-exp/material-ng/src/app/material.ts
@@ -1,9 +1,13 @@
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, FlexLayoutModule, MatIconModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, FlexLayoutModule, MatIconModule],
   })
   export class MaterialModule { }
